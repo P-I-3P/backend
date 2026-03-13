fa2d0708ccf0bc4bfa6b27e12a0fba988ea2fe85
jvxtorr <joaovictortwrp@gmail.com>
2026-03-13 08:38:02 -0300

    Atualização
    Rotas com autenticação configuradas

--- functions/routes/cursosRouter.js
+++ functions/routes/cursosRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
+import { requireAdmin } from "../middlewares/authMiddleware.js";
 import { listarCursos, buscarCurso, criarCurso, atualizarCurso, deletarCurso } from "../controllers/cursosController.js";
 
 const router = express.Router();
 
-router.get("/", listarCursos);
-router.get("/:id", buscarCurso);
-router.post("/", criarCurso);
-router.put("/:id", atualizarCurso);
-router.delete("/:id", deletarCurso);
+router.get("/",...requireAdmin, listarCursos);
+router.get("/:id",...requireAdmin, buscarCurso);
+router.post("/",...requireAdmin, criarCurso);
+router.put("/:id",...requireAdmin, atualizarCurso);
+router.delete("/:id", ...requireAdmin,deletarCurso);
 
 export default router;
